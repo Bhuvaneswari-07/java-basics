3b3de408c06baf3ce3922d89f1f4560018c2a305
Bhuvaneswari-07 <bhuvana2462373@gmail.com>
2026-07-30 21:57:52 +0530

    print hello java

---
+++ TypeConversions.java
@@ -0,0 +1,15 @@
+public class TypeConversions {
+    public static void main(String[] args)
+    {
+        //casting
+        int b=10;
+        byte a=(byte)b;
+        //promotion
+        byte k=10;
+        byte l=20;
+        int result=k*l;
+        System.out.println(result);
+
+        System.out.println(a);
+    }
+}
