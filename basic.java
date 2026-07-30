03343bce9e57458e3929b2863d4349f51a7175d7
Bhuvaneswari-07 <bhuvana2462373@gmail.com>
2026-07-30 21:28:26 +0530

    print hello java

---
+++ basic.java
@@ -0,0 +1,12 @@
+public class basic {
+    public static void main(String[] args)
+    {
+        int a=3;
+        double b=12.5;
+        double result=a+b;
+        int c=0b101;
+        c++;
+        System.out.println(c);
+        System.out.println(result);
+    }
+}
