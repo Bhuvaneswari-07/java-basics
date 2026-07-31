8ce65ab4279f708fb3c5a4bee0f4a5154595da06
Bhuvaneswari-07 <bhuvana2462373@gmail.com>
2026-07-31 21:07:07 +0530

    arithmetic operators

---
+++ Am.java
@@ -0,0 +1,11 @@
+public class Am {
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+        int r=a%b;
+        int n=a++;
+        int m=+a;
+        System.out.println(r);
+        System.out.println(n);
+        System.out.println(m);
+    }}
