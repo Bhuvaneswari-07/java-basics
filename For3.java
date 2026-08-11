6384a0c7fe948bd297d17d701be5e2cfb41fd1c3
Bhuvaneswari-07 <bhuvana2462373@gmail.com>
2026-08-11 17:57:12 +0530

    for loop prime or not

---
+++ For3.java
@@ -0,0 +1,19 @@
+public class For3 {
+    public static void main(String[] args) {
+        int n = 23;
+        int i;
+        for (i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println("not");
+                break;
+            }
+            else{
+
+                    System.out.println("prime");
+                    break;
+                }
+            }
+
+            }
+        }
+
