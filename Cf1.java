f3c9c0d0404de984282cbdfa255095f59d076a1c
Bhuvaneswari-07 <bhuvana2462373@gmail.com>
2026-08-12 21:13:10 +0530

    for loop for frequency

---
+++ Cf1.java
@@ -0,0 +1,27 @@
+public class Cf1 {
+    public static void main(String[] args) {
+
+        int n = 1122333;
+
+        for (int i = 0; i <= 9; i++) {
+
+            int count = 0;
+            int temp = n;
+
+            while (temp > 0) {
+
+                int digit = temp % 10;
+
+                if (digit == i) {
+                    count++;
+                }
+
+                temp = temp / 10;
+            }
+
+            if (count > 0) {
+                System.out.println(i + " -> " + count);
+            }
+        }
+    }
+}
