be74bf6b7b3f993922ea7a081ae350c2c605a151
Bhuvaneswari-07 <bhuvana2462373@gmail.com>
2026-08-13 11:45:46 +0530

    for loop for prime nums

---
+++ Cf2.java
@@ -0,0 +1,19 @@
+public class Cf2 {
+    public static void main(String[] args) {
+
+        for (int n = 1; n < 50; n++) {
+
+            int count = 0;
+
+            for (int i = 2; i <= n; i++) {
+                if (n % i == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println(n);
+            }
+        }
+    }
+}
