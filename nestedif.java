0bc4630bc36672980135928fb4e8b27fbd9ffca6
Bhuvaneswari-07 <bhuvana2462373@gmail.com>
2026-08-16 16:54:52 +0530

    control statements

---
+++ nestedif.java
@@ -0,0 +1,19 @@
+public class nestedif {
+    public static void main(String[] args){
+        for(int i=0;i<3;i++){
+            System.out.print("pass"+i+":");
+            for(int j=0;j<100;j++){
+                if(j==10) {
+                    break;
+                }
+                System.out.print(j+"");
+
+
+
+
+            }
+            System.out.println();
+        }System.out.println("loops complete");
+    }
+}
+
